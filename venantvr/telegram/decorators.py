0ad54f7eed2fe159b2ce3bb5a6fa442768230b50
rvv <r.venant.valery@gmail.com>
2025-08-15 21:42:34 +0200

    Revert ".gitignore..."
    This reverts commit e50b4469580e5e27b1ad3d9c4e0375261b76af05.

--- venantvr/telegram/decorators.py
+++ venantvr/telegram/decorators.py
@@ -1,4 +1,3 @@
-# venantvr/telegram/decorators.py
 from typing import Callable, Dict, List, Optional
 
 from venantvr.telegram.classes.command import Command
@@ -6,26 +5,20 @@ from venantvr.telegram.classes.menu import Menu
 
 COMMAND_REGISTRY: Dict[str, dict] = {}
 
+
 def command(name: str, description: str = "", asks: Optional[List[str]] = None, kwargs_types: Optional[Dict[str, Callable]] = None, menu: Optional[str] = None):
     def decorator(func: Callable):
         command_enum = Command.from_value(name)
-        arg_names = list(func.__code__.co_varnames[1:func.__code__.co_argcount])
-        # Trouver le handler auquel la méthode appartient
-        handler_class = None
-        if hasattr(func, '__self__'):
-            handler_class = func.__self__.__class__
-        elif func.__qualname__.split('.')[:-1]:  # Si c'est une méthode de classe
-            class_name = func.__qualname__.split('.')[-2]
-            handler_class = globals().get(class_name)  # Note : ceci peut nécessiter un ajustement selon votre structure
+        arg_names = list(func.__code__.co_varnames[1:func.__code__.co_argcount])  # Fixed: co_argcount
         COMMAND_REGISTRY[name] = {
             "action": func,
             "enum": command_enum,
             "arg_names": arg_names,
             "asks": asks or [],
             "kwargs_types": kwargs_types or {},
-            "menu": Menu.from_value(menu) if menu else None,
-            "handler": handler_class  # Ajout Ernst: Ajouter le handler à l'entrée du registre
+            "menu": Menu.from_value(menu) if menu else None
         }
-        print(f"Registered command: {name}, Details: {COMMAND_REGISTRY[name]}")
+        print(f"Registered command: {name}, Details: {COMMAND_REGISTRY[name]}")  # Debug print
         return func
+
     return decorator
